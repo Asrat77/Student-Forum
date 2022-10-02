8b0a7f614c29d6543b51b6a38c1b811afbeb5fd2
Asrat77 <asratefrem5@gmail.com>
2022-10-02 18:39:07 +0300

    Added Room views to home page.

--- base/views.py
+++ base/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Let us learn python'},
+    {'id': 2, 'name': 'Web Development'},
+    {'id': 3, 'name': 'Undergraduate Group'},
+]
+
+
 def room(request):
    return  render(request, 'room.html')
 def home(request):
-   return  render(request, 'home.html')
+   context = {'rooms': rooms}
+   
+   return  render(request, 'home.html',context)
     
