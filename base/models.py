70d6c8e5c8f7e7a33c6f024b5d4ae2dee8d34cf5
AsratE <112464138+Asrat77@users.noreply.github.com>
2022-10-04 13:31:23 +0300

    Error fixes and CRUD database initial implimenations

--- base/models.py
+++ base/models.py
@@ -1,20 +1,39 @@
-from turtle import update
-from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Room(models.Moedel):
-    # host= 
-    # topic=
-    # participants=
+class Topic(models.Model):
     
-    name = models.CharField(max_Length=200)
-    description = models.TextField(null=True, blank=True)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
+
+
+class Room(models.Model):
+    
+    host=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic=models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    # participants=
+    
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE )
+    room = models.ForeignKey(Room , on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.body[:50]
--- base/views.py
+++ base/views.py
@@ -1,29 +1,32 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from base.models import Room
 # Create your views here.
 
+'''
 rooms = [
     {'id': 1, 'name': 'Let us learn python'},
     {'id': 2, 'name': 'Web Development'},
     {'id': 3, 'name': 'Undergraduate Group'},
 ]
-
+'''
 
 def room(request, pk):
    
-   room = None
-   
-   for i in rooms:
-      
-      if i['id'] == int(pk):
-         room = i
-      context = {'room': room}
+   room = Room.objects.get(id=pk)
+   context = {'room': room}
    
    return  render(request, 'base/room.html', context)
 
 def home(request):
-   
+   rooms = Room.objects.all()
    context = {'rooms': rooms}
    
    return  render(request, 'base/home.html',context)
-    
+
+
+def createRoom(request):
+   context = {}
+   return render(request, 'base/room_form.html', context)
