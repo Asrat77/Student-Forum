3ea0d3fd592de8e4eeed3afb284cccc1f3bb077d
AsratE <112464138+Asrat77@users.noreply.github.com>
2022-10-04 18:54:13 +0300

    'Database development'

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 from base.models import Room
+from .forms import RoomForm
 # Create your views here.
 
 '''
@@ -23,10 +23,41 @@ def room(request, pk):
 def home(request):
    rooms = Room.objects.all()
    context = {'rooms': rooms}
-   
+
    return  render(request, 'base/home.html',context)
 
 
 def createRoom(request):
-   context = {}
+   form = RoomForm()
+
+   context = {'form': form}
+   if request.method == 'POST':
+      form = RoomForm(request.POST)
+      if form.is_valid():
+         form.save()
+      return redirect('home')
    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+   
+   
+   room = Room.objects.get(id=pk) 
+   form = RoomForm(instance=room)
+   context = {'form': form}
+
+   if request.method == 'POST':
+      form = RoomForm(request.POST, instance=room)
+      if form.is_valid():
+         form.save()
+      return redirect('home')
+   return render(request,'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+   room = Room.objects.get(id=pk)
+   
+   if request.method == 'POST':
+      room.delete()
+      return redirect('home')
+   return render(request,'base/delete.html', {'obj':room})
+
+
